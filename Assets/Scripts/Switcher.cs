82b8a5ae12216cabe9ee862febf8df07bf9776c5
Andrew Tymoshenko <andrei.tymoshenko@gmail.com>
2020-09-14 23:36:02 +0300

    hard gang bang between 38 men

---
+++ Assets/Scripts/Switcher.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class Switcher : MonoBehaviour
+{
+    public GameObject[] lines;
+    public float animationSpeed = 1f;
+    public Transform handle;
+    public AudioSource onAudio;
+    public AudioSource offAudio;
+
+    bool on = false;
+    bool onAnimation = false;
+    float time = 0f;
+    Quaternion startAngle = Quaternion.Euler(0, -90.001f, 45);
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!gameObject.GetComponent<Collider>())
+            gameObject.AddComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onAnimation)
+        {
+            if (on)
+            {
+                Quaternion endAngle = Quaternion.Euler(0, -90.001f, -25);
+                //onAudio?.Play();
+                if (time <= animationSpeed)
+                {
+                    handle.localRotation = Quaternion.Slerp(startAngle, endAngle, time / animationSpeed);
+                    time += Time.deltaTime;
+                }
+                else
+                {
+                    handle.localRotation = endAngle;
+                    startAngle = endAngle;
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+
+                        lines[i]?.GetComponent<Renderer>().sharedMaterial.EnableKeyword("_EMISSION");
+                    }
+                    onAnimation = false;
+                }
+            }
+            else
+            {
+                Quaternion endAngle = Quaternion.Euler(0, -90.001f, 45);
+                //offAudio?.Play();
+                if (time <= animationSpeed)
+                {
+                    handle.localRotation = Quaternion.Slerp(startAngle, endAngle, time / animationSpeed);
+                    time += Time.deltaTime;
+                }
+                else
+                {
+                    handle.localRotation = endAngle;
+                    startAngle = endAngle;
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        lines[i]?.GetComponent<Renderer>().sharedMaterial.DisableKeyword("_EMISSION");
+                    }
+                    onAnimation = false;
+                }
+            }
+
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!onAnimation)
+        {
+            on = !on;
+            time = 0f;
+            onAnimation = true;
+        }
+    }
+
+}
